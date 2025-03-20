45bca1c8f7c47ce149f517adae07df648b5fcae1
muratq <okumusmurat74@gmail.com>
2025-03-20 23:18:06 +0300

    video oynatma ve kamera calistirma eklendi

--- src/Main.cpp
+++ src/Main.cpp
@@ -14,7 +14,7 @@ void GorselOkuGoster(string dosyaYolu)
         cout<<"dosya okuma basarili"<<endl;
         cv::namedWindow("ekran"); // pencere olusturduk
         cv::imshow("ekran",resim); // ekrana bastir
-        cv::waitKey(1); // tusa basilana kadar bekle
+        cv::waitKey(0); // tusa basilana kadar bekle
 
     }
     else
@@ -23,10 +23,81 @@ void GorselOkuGoster(string dosyaYolu)
     }
 }
 
+void videoOkuGoster(string dosyaYolu)
+{
+   cv::VideoCapture video;
+   video.open(dosyaYolu);
+   if(video.isOpened())
+   {
+        cout<<"video acildi!"<<endl;
+        cv::Mat anlik_resim;
+        cv::namedWindow("ekran");
+        int fps =0;
+        int tus =0;
+        fps =10;
+        while(video.read(anlik_resim)) //video okunuyosa anlik resme framei at
+        {
+           cv::imshow("ekran",anlik_resim);
+           tus = cv::waitKey(fps);
+           if(tus == 27) // fps verdik
+           {
+                break;
+           }
+           else if(tus == 43)
+           {
+                fps++;
+                cout<<"fps degeri :"<<fps<<endl;
+           }
+        }
+        cv::destroyAllWindows();
+        cout<<"video sonlandirildi"<<endl;
+
+
+   }
+   else
+   {
+        cout<<"video acilamadi"<<endl;
+   }
+   cv::waitKey();
+
+}
+
+void webcamOkuGoster(int ch)
+{
+    cv::VideoCapture video;
+    
+    if(video.open(ch))
+    {
+        cout<<"webcam acildi"<<endl;
+        cv::Mat anlik_goruntu;
+        cv::namedWindow("ekran");
+        while(video.grab())
+        {
+            video.retrieve(anlik_goruntu);
+            cv::imshow("ekran",anlik_goruntu);
+            if(cv::waitKey(25) == 27)
+            {
+                break;
+            }
+        }
+        cv::destroyAllWindows();
+
+    }
+
+
+
+}
+
+
+
+
 
 int main()
 {
+    webcamOkuGoster(0);
+    int a  =0;
 
-    GorselOkuGoster("C:/Users/murat/opencvtest/src/manzara.jpg");
+
+    
 
 }
